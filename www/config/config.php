535075719f06abd48e071acfd708ad40966a26e4
liubinbin <lotosbin@gmail.com>
2015-10-28 18:14:52 +0800

    git ignore;docker-compose.yml

--- www/config/config.php
+++ www/config/config.php
@@ -1,1 +1,35 @@
-//网站配置
+<?php
+return array(
+	'logs'=>array(
+		'path'=>'backup/logs/log',
+		'type'=>'file'
+	),
+	'DB'=>array(
+		'type'=>'mysqli',
+        'tablePre'=>'iwebshop_',
+		'read'=>array(
+			array('host'=>'mysql:3306','user'=>'root','passwd'=>'123456','name'=>'iwebshop'),
+		),
+
+		'write'=>array(
+			'host'=>'mysql:3306','user'=>'root','passwd'=>'123456','name'=>'iwebshop',
+		),
+	),
+	'interceptor' => array('themeroute@onCreateController','layoutroute@onCreateView','hookCreateAction@onCreateAction','hookFinishAction@onFinishAction'),
+	'langPath' => 'language',
+	'viewPath' => 'views',
+	'skinPath' => 'skin',
+    'classes' => 'classes.*',
+    'rewriteRule' =>'url',
+	'theme' => array('pc' => 'default','mobile' => 'mobile'),
+	'skin' => array('pc' => 'default','mobile' => 'default'),
+	'timezone'	=> 'Etc/GMT-8',
+	'upload' => 'upload',
+	'dbbackup' => 'backup/database',
+	'safe' => 'cookie',
+	'lang' => 'zh_sc',
+	'debug'=> false,
+	'configExt'=> array('site_config'=>'config/site_config.php'),
+	'encryptKey'=>'fbe28e5fc26046de656d03867861b203',
+);
+?>
